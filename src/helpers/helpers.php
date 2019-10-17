4ea87dd4282fe443837d8db538196b6e430b86dc
Gustavo Torres <gustavo.torres@invisual.com.br>
2019-10-16 22:57:59 -0300

    criada função global (helper) abort(); Para tratar interrupções no código com envio de HTTPStatusCode.

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -14,3 +14,17 @@
  function dd($variavel){
     var_dump($variavel); die();
  }
+
+ /**
+  * Função responsável por encerrar a execução da aplicação retornando um status code HTTP específico
+  *
+  * @param integer $httpStatusCode
+  * @param string $strMensagem
+  * @return void
+  */
+ function abort(int $httpStatusCode, $strMensagem)
+ {
+    // header('HTTP/1.0 404 Not Found');
+    http_response_code($httpStatusCode);
+    echo $strMensagem;
+ }
