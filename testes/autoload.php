36d4ab7fe51599c9bc3711fc0d43c7c236994446
Gustavo Torres <gustavo.torres@invisual.com.br>
2019-10-16 21:29:20 -0300

    Criando estrutura de autoload. Utilizando autoload do composer, por ser o mais indicado segundo os padrões.

---
+++ testes/autoload.php
@@ -0,0 +1,15 @@
+<?php
+
+//Autoload
+
+// https://www.php.net/manual/pt_BR/language.oop5.autoload.php
+
+// https://medium.com/weyes/fazendo-autoload-de-classes-no-php-c802623adeaf
+
+// http://www.kadunew.com/blog/php/funcao-autoload-no-php
+
+// https://patriciahillebrandt.com/php-como-fazer-o-autoload-de-classes-usando-o-composer/
+
+// http://www.douglaspasqua.com/2015/01/26/php-autoload-psr-4psr-0-com-composer/
+
+// https://patriciahillebrandt.com/php-como-fazer-o-autoload-de-classes-usando-o-composer/
