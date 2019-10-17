8f76b7036967d17ed058614aef67cea6a5d51304
Gustavo Torres <gustavo.torres@invisual.com.br>
2019-10-16 21:30:23 -0300

    Testando autoload com acesso aos controllers.

--- index.php
+++ index.php
@@ -4,4 +4,11 @@
  * Arquivo responsável por inicializar todo o sistema
  */
 
- var_dump($_REQUEST);
+require_once __DIR__ . '/vendor/autoload.php';
+
+// var_dump($_REQUEST);
+
+use Viking\Controllers\TesteController;
+
+$testeController = new TesteController();
+$testeController->testar();
