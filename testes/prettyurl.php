0a93cad8c35ded947182c83087cfa8fce04ab2e4
Gustavo Torres <gustavo.torres@invisual.com.br>
2019-10-16 21:23:10 -0300

    Anotações em testes/prettyurl

--- testes/prettyurl.php
+++ testes/prettyurl.php
@@ -3,3 +3,11 @@
 //Pretty Url
 
 //https://web.archive.org/web/20140220071025/http://www.phpriot.com/articles/search-engine-urls/2
+
+#RewriteEngine on
+#RewriteRule ^index.html$ testes/index2.html
+
+#RewriteEngine on
+#RewriteRule ^/news/([0-9]+)\.html /news.php?news_id=$1&%{QUERY_STRING}
+
+#RewriteRule ^(.*)$ /index.php
