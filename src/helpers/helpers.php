7b4b5bdd52c0adbb05e81c2578792e86496858c8
Gustavo Torres <gustavo.torres@invisual.com.br>
2019-10-18 14:25:25 -0300

    Apenas identação e formatação do código helpers.php

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -5,26 +5,28 @@
  * que estarão disponíveis para toda a aplicação
  */
 
- /**
-  * Função Dump And Die
-  *
-  * @param mixed $variavel
-  * @return void
-  */
- function dd($variavel){
-    var_dump($variavel); die();
- }
+/**
+ * Função Dump And Die
+ *
+ * @param mixed $variavel
+ * @return void
+ */
+function dd($variavel)
+{
+   var_dump($variavel);
+   die();
+}
 
- /**
-  * Função responsável por encerrar a execução da aplicação retornando um status code HTTP específico
-  *
-  * @param integer $httpStatusCode
-  * @param string $strMensagem
-  * @return void
-  */
- function abort(int $httpStatusCode, $strMensagem)
- {
-    // header('HTTP/1.0 404 Not Found');
-    http_response_code($httpStatusCode);
-    die($strMensagem);
- }
+/**
+ * Função responsável por encerrar a execução da aplicação retornando um status code HTTP específico
+ *
+ * @param integer $httpStatusCode
+ * @param string $strMensagem
+ * @return void
+ */
+function abort(int $httpStatusCode, $strMensagem)
+{
+   // header('HTTP/1.0 404 Not Found');
+   http_response_code($httpStatusCode);
+   die($strMensagem);
+}
