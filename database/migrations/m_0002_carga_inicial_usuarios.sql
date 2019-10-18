eb0361fe98873959c6af87a776e9b6a0fb377deb
Gustavo Torres <gustavo.torres@invisual.com.br>
2019-10-18 13:37:31 -0300

    Incluindo carga inicial de usuários

---
+++ database/migrations/m_0002_carga_inicial_usuarios.sql
@@ -0,0 +1,2 @@
+INSERT INTO `usuarios` (`nome`, `email`, `login`, `senha`) VALUES ('admin', 'admin@example.com', 'admin', '123456');
+INSERT INTO `usuarios` (`nome`, `email`, `login`, `senha`) VALUES ('secretaria', 'secretaria@example.com', 'secretaria', '123456');
