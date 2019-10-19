1941e6b25e4516e132baed4fe332599af120740c
Gustavo Torres <gustavo.torres@invisual.com.br>
2019-10-19 08:10:41 -0300

    Alterando estrutura de redirecionamento de diretorios e arquivos para comportar o acesso à pasta public diretamente acessando o diretorio raiz da aplicação.

--- public/index.php
+++ public/index.php
@@ -5,8 +5,9 @@
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/helpers/helpers.php';
+require_once '../src/helpers/helpers.php';
 
+// var_dump($_REQUEST);
 session_start();
 
 use Viking\Routes\Routes;
@@ -17,7 +18,7 @@ unset($queryItens['rota']);
 
 try {
     $routes = new Routes();
-    $routes->direcionarRequisicao($_SERVER['REQUEST_METHOD'], $_REQUEST['rota'], $queryItens);
+    $routes->direcionarRequisicao($_SERVER['REQUEST_METHOD'], $_REQUEST['rota'] ?? '', $queryItens);
 } catch (\Throwable $th) {
     dd($th);
 }
