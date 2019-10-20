d2832230df29cd18a228df19141c330297046083
Gustavo Torres <gustavo.torres@invisual.com.br>
2019-10-20 12:40:42 -0300

    Alterada página de login, formatação de código e adicionada mensagem em caso de submeter credenciais incorretas.

--- src/views/paginas/login.php
+++ src/views/paginas/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,62 +17,70 @@
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
+
 <body class="hold-transition login-page" style="background-color:#000">
-<div class="login-box">
-  <div class="login-logo">
-    <img src="img/vikingslogo160.png" alt="" style="max-width: -webkit-fill-available;">
-    <!-- <a href="index2.html"><b>Vi</b>Kings</a> -->
-  </div>
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body" style="background-color: #2b2b2b;background-image: linear-gradient(#000,#2b2b2b);">
-
-      <form action="" method="post">
-        <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email" name="login">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="senha" name="senha">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-
-      <div class="social-auth-links text-center mb-3">
-        <p>- OU -</p>
-        <a href="login/recuperar-senha" class="btn btn-block btn-primary">
-          <i class="fa fa-reload"></i> Esqueci minha senha
-        </a>
-      </div>
+  <div class="login-box">
+    <div class="login-logo">
+      <img src="img/vikingslogo160.png" alt="" style="max-width: -webkit-fill-available;">
+      <!-- <a href="index2.html"><b>Vi</b>Kings</a> -->
     </div>
-    <!-- /.login-card-body -->
-  </div>
-</div>
-<!-- /.login-box -->
+    <!-- /.login-logo -->
+    <?php if($erroDeCredencial ?? false) {?>
+    <div class="alert alert-danger alert-dismissible">
+      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+      <h5><i class="icon fas fa-exclamation"></i> Login e/ou senha incorretos!</h5>
+    </div>
+    <?php } ?>
+    <div class="card">
+      <div class="card-body login-card-body" style="background-color: #2b2b2b;background-image: linear-gradient(#000,#2b2b2b);">
 
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="js/adminlte.min.js"></script>
+        <form action="" method="post">
+          <div class="input-group mb-3">
+            <input type="email" class="form-control" placeholder="Email" name="login">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-envelope"></span>
+              </div>
+            </div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="senha" name="senha">
+            <div class="input-group-append">
+              <div class="input-group-text">
+                <span class="fas fa-lock"></span>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-8">
+            </div>
+            <!-- /.col -->
+            <div class="col-4">
+              <button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
+            </div>
+            <!-- /.col -->
+          </div>
+        </form>
+
+        <div class="social-auth-links text-center mb-3">
+          <p>- OU -</p>
+          <a href="login/recuperar-senha" class="btn btn-block btn-primary">
+            <i class="fa fa-reload"></i> Esqueci minha senha
+          </a>
+        </div>
+      </div>
+      <!-- /.login-card-body -->
+    </div>
+  </div>
+  <!-- /.login-box -->
+
+  <!-- jQuery -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="js/adminlte.min.js"></script>
 
 </body>
+
 </html>
