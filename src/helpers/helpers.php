d8c34a6efb43d3d5450114e8ad3cae15018c5939
Gustavo Torres <gustavo.torres@invisual.com.br>
2019-10-20 12:36:25 -0300

    Adicionandas funções helpers

--- src/helpers/helpers.php
+++ src/helpers/helpers.php
@@ -5,6 +5,11 @@
  * que estarão disponíveis para toda a aplicação
  */
 
+use Viking\Auth\Auth;
+use Viking\Config\Config;
+use Viking\Routes\Routes;
+use Viking\Views\View;
+
 /**
  * Função Dump And Die
  *
@@ -29,4 +34,45 @@ function abort(int $httpStatusCode, $strMensagem)
    // header('HTTP/1.0 404 Not Found');
    http_response_code($httpStatusCode);
    die($strMensagem);
+}
+
+/**
+ * Função responsável por retornar o usuário logado
+ *
+ * @return Usuario 
+ */
+function authuser()
+{
+   return Auth::user();
+}
+
+/**
+ * Funçõ para facilitar a construção de páginas por pedaços
+ *
+ * @param [type] $endereco
+ * @return void
+ */
+function getPageContent($endereco)
+{
+   return file_get_contents(Config::getBasePath() . '/src/views/paginas/' . $endereco . '.php');
+}
+
+function includeViewPiece($endereco, $arrVars = [])
+{
+   return View::incluir($endereco, $arrVars);
+}
+
+function appbasepath()
+{
+   return Config::getBasePath();
+}
+
+function appbaseurl()
+{
+   return Routes::getBaseURL();
+}
+
+function getconfig($param)
+{
+   return Config::getEnv($param) ?? '';
 }
