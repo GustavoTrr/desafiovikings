3b4592cb85bd930244586c9228e382e49fc89903
Gustavo Torres <gustavo.torres@invisual.com.br>
2019-10-22 04:36:20 -0300

    Inclusão de funcionalidades no AUth.

--- src/auth/Auth.php
+++ src/auth/Auth.php
@@ -2,6 +2,7 @@
 
 namespace Viking\Auth;
 
+use Viking\Mail\Mailer;
 use Viking\Models\Usuario;
 use Viking\Routes\Routes;
 
@@ -29,7 +30,7 @@ class Auth {
 
     public static function logar(string $login, string $senha)
     {
-        $usuarioEncontrado = Usuario::where(['email' => $login, 'senha' => $senha])->one();
+        $usuarioEncontrado = Usuario::where(['email' => $login, 'senha' => self::criptografarSenha($senha)])->one();
         if (!empty($usuarioEncontrado)) {
             $_SESSION['idUsuarioLogado'] = $usuarioEncontrado->id;
             return true;
@@ -52,4 +53,49 @@ class Auth {
     {
         return (!empty($_SESSION['idUsuarioLogado']));
     }
+
+    public static function recuperarSenha($email)
+    {
+        $usuario = Usuario::where(['email' => $email])->one();
+        if (!empty($usuario)) {
+            $novaSenhaTemporaria = $usuario->alterarSenhaAutomaticamente();
+            try {
+                Mailer::enviarEmail([$email], 'Recuperação de Senha', 'Sua nova senha temporária é "' . $novaSenhaTemporaria . '" (sem as aspas). <p> Recomendamos trocar a senha assim que entrar no sistema.</p><p><a href="'.appbaseurl().'">'.appbaseurl().'</a></p>');
+            } catch (\Throwable $th) {
+                throw new \Exception("Erro ao tentar enviar o email de recuperação de senha. :: " . $th->getMessage(), 1);
+                
+            }
+            
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $senhaAntiga
+     * @param [type] $senhaNova
+     * @return void
+     */
+    public static function atualizarSenha($senhaAntiga, $senhaNova)
+    {
+        $usuario = self::user();
+        
+        if ($usuario->senha == password_hash($senhaAntiga,PASSWORD_BCRYPT)) {
+            $usuario->update(['senha' => $senhaNova]);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function criptografarSenha($senha)
+    {
+        return Usuario::criptografarSenha($senha);
+    }
+
+    
 }
